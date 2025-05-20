a67a9aca15c2b10e19a34b12b15b5931ea14fcce
Willium1925 <d1397026@o365.fcu.edu.tw>
2025-05-20 15:48:02 +0800

    安裝rss-parser

--- http.js
+++ http.js
@@ -11,3 +11,20 @@ http.createServer(function (req, res) {
 }).listen(8080);
 
 console.log("本機端的網址: http://localhost:8080");
+
+
+import Parser from 'rss-parser';
+let parser = new Parser();
+// 定義一個非同步函數
+async function fetchData() {
+    let feed = await parser.parseURL('https://www.cwa.gov.tw/rss/Data/cwa_warning.xml');
+    console.log(feed.title);
+    feed.items.forEach(item => {
+
+        console.log(item.title + ':' + item.link+':'+item.content);
+    });
+
+}
+// 調用非同步函式
+fetchData();
+console.log("立刻執行");
